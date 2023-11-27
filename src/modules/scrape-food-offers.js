89d4998956f53cc777d2debbcaf1979937b8b54e
Jan.Parve <jan.parve@energia.ee>
2023-11-27 18:37:18 +0200

    update rotermanni offer selecting

--- src/modules/scrape-food-offers.js
+++ src/modules/scrape-food-offers.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const cheerio = require('cheerio')
 const puppeteer = require('puppeteer')
 
-const ROTERMANN_URL = 'https://www.rotermann.eu/lounapakkumised/'
+const ROTERMANN_URL = 'https://rotermann.ee/tana-lounaks/'
 const BASIILIK_URL = 'https://basiilik.ee/en/daily-specials/'
 
 const BLACKLIST = [
@@ -25,33 +25,28 @@ const DAYS_OF_WEEK = [
   'Reede'
 ]
 
-async function getRotermanniLunchOffers() {
+async function getRotermanniLunchOffers () {
   const menus = {}
   const response = await axios(ROTERMANN_URL)
   const html = response.data
   const $ = cheerio.load(html)
 
-  const selectedElem = 'body > main > div > div.row.gutter-20 > div:nth-child(1) > div > div'
+  const selectedElem = '.lunch > .lunch--inner'
 
   $(selectedElem).each((parentIndex, parentElem) => {
-    $(parentElem)
-      .children()
-      .each((childId, childElem) => {
-        const value = $(childElem).text()
-          .split('\n')
-          .map((item) => item.trim())
-          .filter((item) => item !== '')
-          .filter((item, i) => item.startsWith('-') || item.startsWith('–') || i === 0)
+    const restaurant = $(parentElem).find('.lunch--title > a').text().trim()
+    const offers = $(parentElem).find('.single-offer > div').map((t, el) => {
+      return `-${$(el).find('.single-offer--content > p').text()} ${$(el).find('.single-offer--price > p').text()}`
+    }).toArray()
 
-        if (BLACKLIST.findIndex((s) => value[0].includes(s)) === -1) {
-          menus[value[0]] = value.slice(1)
-        }
-      })
+    if (BLACKLIST.findIndex((s) => restaurant.includes(s)) === -1 && restaurant !== '' && offers.length) {
+      menus[restaurant] = offers
+    }
   })
   return menus
 }
 
-async function getBasiilikLunchOffer() {
+async function getBasiilikLunchOffer () {
   const offers = []
   const browser = await puppeteer.launch({ headless: 'new', args: ['--no-sandbox'] })
   const page = await browser.newPage()
@@ -76,7 +71,7 @@ async function getBasiilikLunchOffer() {
   return offers.map((o) => `-${o}`)
 }
 
-async function getLunchOffers() {
+async function getLunchOffers () {
   const menus = await getRotermanniLunchOffers()
   menus.Basiilik = await getBasiilikLunchOffer()
 
